6c6d3734b39de9e91771bdb4d15ad437b4ebdd11
AntoineMtbn <antoine.montauban@gmail.com>
2023-05-25 17:25:35 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { UserProfileComponent } from './user-profile/user-profile.component';
+
+import { SignupComponent } from './signup/signup.component';
+
+const ROUTES: Routes = [
+  //{ path: 'home', component: HomeComponent },
+
+  //{ path: 'contact', component: ContactComponent },
+
+  { path: 'user', component: UserProfileComponent },
+
+  { path: 'signup', component: SignupComponent },
+];
+
+export { ROUTES };
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(ROUTES)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule } from '@angular/router';
+
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 
+
+import { HomeComponent } from './home/home.component';
+
+import { ContactComponent } from './contact/contact.component';
+
+import { ROUTES } from './app-routing.module';
+import { SignupComponent } from './signup/signup.component';
+import { MenuComponent } from './menu/menu.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     UserProfileComponent,
+    HomeComponent,
+    ContactComponent,
+    SignupComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
